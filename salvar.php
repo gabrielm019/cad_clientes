8062a0769360450521534510c3aa9c9cbc33c0ba
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-04-05 21:14:10 -0300

    .

---
+++ salvar.php
@@ -0,0 +1,19 @@
+<?php
+$nome = $_POST['nome'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+
+$arquivo = fopen("cliente.csv", "a");
+
+fwrite($arquivo, $nome . ",");
+fwrite($arquivo, $cidade . ",");
+fwrite($arquivo, $estado . ",");
+fwrite($arquivo, $telefone . ",");
+fwrite($arquivo, $email . "\n");
+
+fclose($arquivo);
+
+header("location:index.php");
+?>
